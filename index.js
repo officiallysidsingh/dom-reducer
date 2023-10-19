b4c871c95f338f49432f6dda44c7422dd776dfa7
officiallysidsingh <siddharthsingh2014@gmail.com>
2023-10-19 12:17:25 +0530

    used puppeteer to scrape a website and logged out profile data like profileImage, fullName, username etc

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
-import express from "express";
-const app = express();
-const PORT = 8000;
+// import express from "express";
+// const app = express();
+// const PORT = 8000;
+//
+// app.get("/", (req, res) => {
+//   res.send("Hello World!!");
+// });
+//
+// app.listen(PORT, () => {
+//   console.log(`Server Ready At PORT: ${PORT}`);
+// });
 
-app.get("/", (req, res) => {
-  res.send("Hello World!!");
-});
+import puppeteer from "puppeteer";
 
-app.listen(PORT, () => {
-  console.log(`Server Ready At PORT: ${PORT}`);
-});
+const url = "https://github.com/kaavee315";
+
+const main = async () => {
+  // Launch puppeteer browser instance
+  const browser = await puppeteer.launch({ headless: "new" });
+
+  // Open blank page in browser instance
+  const page = await browser.newPage();
+
+  // Navigate page to URL
+  await page.goto(url);
+
+  // Perform HTML DOM Manipulations
+  const reducedDOM = await page.evaluate(() => {
+    // Getting the profile data
+    const profileData = document.querySelector(
+      "div.js-profile-editable-replace",
+    );
+
+    // Getting important profile data
+    const profileImage = profileData.querySelector("img.avatar-user").src;
+    const fullname = profileData.querySelector("span.p-name").innerText;
+    const username = profileData.querySelector("span.p-nickname").innerText;
+
+    return { profileImage, fullname, username };
+  });
+
+  console.log(reducedDOM);
+
+  // Close the browser instance with all associated pages
+  await browser.close();
+};
+
+main();
